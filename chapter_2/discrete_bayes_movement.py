a701f600baa617cf1b88979df3e9b79e56d7bb40
crvogt <crvogt26@gmail.com>
2024-06-18 16:54:13 -0700

    It's probably obvious to someone

---
+++ chapter_2/discrete_bayes_movement.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, axs = plt.subplot(1, 1)
+
+
--- chapter_2/discrete_bayes_simple.py
+++ chapter_2/discrete_bayes_simple.py
@@ -14,6 +14,30 @@ def scaled_update(hall, belief, z, z_prob):
     belief[hall==z] *= scale
     return normalize(belief)
 
+def scaled_update_terms(hall, belief, z, z_prob):
+    scale = z_prob / (1 - z_prob)
+    likelihood = np.ones(len(hall))
+    likelihood[hall==z] *= scale
+    return normalize(likelihood * belief)
+
+def update(likelihood, prior):
+    # This function is meant to be more generalized than the previous two
+    return normalize(likelihood * prior)
+
+def lh_hallway(hall, z, z_prob):
+    """
+    Compute likelihood that a measurement matches
+    positions in the hallway
+    """
+    try:
+        scale = z_prob / (1. - z_prob)
+    except ZeroDivisionError:
+        scale = 1e8
+
+    likelihood = np.ones(len(hall))
+    likelihood[hall==z] *= scale
+    return likelihood
+
 ############# Code Body ############
 # This is called the prior, our initial belief
 belief = np.array([0.1] * 10)
@@ -23,6 +47,11 @@ hallway = np.array([1, 1, 0, 0, 0, 0, 0, 0, 1, 0])
 
 # This is a measurement
 reading = 1
+
+likelihood = lh_hallway(hallway, reading, 0.75)
+
+posterior = update(likelihood, belief)
+print(posterior)
 
 # We add correct scale as 3, because "through testing"
 # we assume a door positive reading is 3x more likely to
@@ -34,18 +63,20 @@ reading = 1
 # door_belief = np.array([1/3, 1/3, 0, 0, 0, 0, 0, 0, 1/3, 0])
 
 # scaled_update updates our belief and normalizes
-belief = scaled_update(hallway, belief, reading, 0.75)
+# belief = scaled_update(hallway, belief, reading, 0.75)
+likelihood = scaled_update_terms(hallway, belief, reading, 0.75)
 
 print(sum(belief))
 
 ########## Setup for plotting ##########
-fig, axs = plt.subplots(1, 1)
+fig, axs = plt.subplots(2, 1)
 x = np.linspace(0, len(hallway) - 1, len(hallway)).astype(int)
-axs.set_ylim(0, 0.3)
-axs.set_xlim(-1, 10)
-axs.grid()
+axs[0].set_ylim(0, 0.3)
+axs[0].set_xlim(-1, 10)
+axs[0].grid()
 
 # This plot can be called a categorical distribution, or multimodal 
 # distribution
-axs.bar(x, belief)
+axs[0].bar(x, belief)
+axs[1].bar(x, likelihood)
 plt.show()
