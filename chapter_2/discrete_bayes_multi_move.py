b772a996c3679cb9b04c7a924de0561bdf82077f
crvogt <crvogt26@gmail.com>
2024-06-24 14:25:23 -0700

    Still updating chapter 2

--- chapter_2/discrete_bayes_multi_move.py
+++ chapter_2/discrete_bayes_multi_move.py
@@ -9,7 +9,19 @@ def update(likelihood, prior):
 def normalize(value):
     return value/sum(value)
 
-#def predict_move_convolution
+# Convolution prediction
+def predict(pdf, offset, kernel):
+    N = len(pdf)
+    kN = len(kernel)
+
+    width = int((kN - 1) / 2)
+
+    prior = np.zeros(N)
+    for i in range(N):
+        for k in range(kN):
+            index = (i + (width - k) - offset % N)
+            prior[i] += pdf[index] * kernel[k]
+    return prior
 
 def lh_hallway(hall, z, z_prob):
     try:
@@ -31,13 +43,16 @@ prior = np.array([0.1] * 10)
 likelihood = lh_hallway(hallway, z=1, z_prob=0.75)
 
 posterior = update(likelihood, prior)
-print(posterior)
+
+kernel = [0.1, 0.8, 0.1]
+
+prior = predict(posterior, 1, kernel)
 
 x = np.linspace(0, len(hallway) - 1, len(hallway))
 
 axs[0].bar(x, prior)
-axs[0].set_ylim(0, 1)
+axs[0].set_ylim(0, 0.3)
 axs[1].bar(x, posterior)
-axs[1].set_ylim(0, 1)
+axs[1].set_ylim(0, 0.3)
 
 plt.show()
